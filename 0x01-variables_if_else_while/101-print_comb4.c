77ebc903351918071ef550bf3a46e3a803255330
Your Name <you@example.com>
2023-03-17 16:12:50 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,22 +10,23 @@
 int main(void)
 
 {
-	int d, p;
+
+	int d, p, q;
 
 	for (d = '0'; d < '9'; d++)
 	{
-		for (p = d + 1; p <= '9'; p++)
-		{
-			if (p != d)
-		{
-		putchar(d);
-		putchar(p);
-		if (d == '8' && p == '9')
-		continue;
-		putchar(',');
-		putchar(' ');
-		}
-		}
+	for (p = d + 1; p <= '9'; p++)
+	{
+	for (q = p + 1; q <= '9'; q++)
+	{
+	if (d == '7' && q == '8')
+	{
+	continue;
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	}
 	}
 putchar('\n');
 return (0);
