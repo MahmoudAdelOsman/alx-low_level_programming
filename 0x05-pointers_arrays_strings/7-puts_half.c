d58ba0dae0a5e9bf25339ac77b7ca9d94ddc835e
Your Name <you@example.com>
2023-03-27 13:22:03 -0700

    7-puts_half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -6,22 +6,21 @@
   * @str: input
   * Return: half of input
   */
-
 void puts_half(char *str)
-
 {
 	int a, n, i;
 
-	longi = 0;
+	i = 0;
+
 	for (a = 0; str[a] != '\0'; a++)
 		i++;
 
 	n = (i / 2);
 
 	if ((i % 2) == 1)
-		n = ((i + 1) / 2)
+		n = ((i + 1) / 2);
 
-			for (a = n; str[a] != '\0'; a++)
-				_putchar(str[a]);
+	for (a = n; str[a] != '\0'; a++)
+		_putchar(str[a]);
 	_putchar('\n');
 }
