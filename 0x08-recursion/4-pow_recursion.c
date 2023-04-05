9277dff87170be0ee31e23d56707ea19404af4c7
Your Name <you@example.com>
2023-04-05 14:04:42 -0700

    0x08. C - Recursion

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -2,8 +2,8 @@
 
 /**
  *  _pow_recursion - Returns the value of x raised to the power of y
- * @
- * @
+ * @x: The value to multiplay
+ * @y: The times to multiplay the value
  * Return: The value multiplied y times
  */
 int _pow_recursion(int x, int y)
