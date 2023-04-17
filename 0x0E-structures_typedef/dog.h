a55d012501b02ffffdf6d08208388b801b9eaba8
Your Name <you@example.com>
2023-04-17 15:42:17 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,31 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+  * struct dog - info about dog
+  * @name: 1st member
+  * @age: 2nd member
+  * @owner: 3rd member
+  * Description: longer description
+  */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+
+typedef struct dog dog_t;
+
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
+
+#endif
