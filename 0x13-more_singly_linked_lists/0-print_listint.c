c3bc3f58521a29cfa4f39b643a4b117adb165f47
MahmoudAdelOsman <mahmoud.adel.elgandy@gmail.com>
2023-04-29 09:52:41 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -3,9 +3,10 @@
 /**
   * print_listint - Prints all the elements of a linked list
   * @x: Linked list of type listint_t to print
+  *
   * Return: Number of nodes
   */
-size_t print_listint (const listint_t *x)
+size_t print_listint(const listint_t *x)
 {
 	size_t num = 0;
 
@@ -18,4 +19,3 @@ size_t print_listint (const listint_t *x)
 
 	return (num);
 }
-/*prints all the elements of a listint_t list.*/
