32dbff85a6e6d5cef1092de2321b3c5279fcb99b
MahmoudAdelOsman <mahmoud.adel.elgandy@gmail.com>
2023-05-02 08:36:45 -0700

    1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -7,13 +7,10 @@
  */
 size_t listint_len(const listint_t *h)
 {
-	size_t num = 0;
+	size_t x;
 
-	while (h)
-	{
-		num++;
+	for (x = 0; h; x++)
 		h = h->next;
-	}
 
-	return (num);
+	return (x);
 }
