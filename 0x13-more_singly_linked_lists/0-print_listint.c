3a1e08627a703ab2d27a8c1ccd724c08642fae89
MahmoudAdelOsman <mahmoud.adel.elgandy@gmail.com>
2023-05-02 03:00:12 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,13 +8,13 @@
   */
 size_t print_listint(const listint_t *h)
 {
-	size_t num = 1;
+	size_t num = 0;
 
 	while (h)
 	{
-		num++;
 		printf("%d\n", h->n);
 		h = h->next;
+		num++;
 	}
 
 	return (num);
