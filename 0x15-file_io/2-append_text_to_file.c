4272f4b25728441a3d7fcb95ce0565358cdd9102
MahmoudAdelOsman <mahmoud.adel.elgandy@gmail.com>
2023-05-08 16:22:45 -0700

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -17,10 +17,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 
 	if (text_content != NULL)
-	{
-		for (s = 0; text_content[s];)
-			s++;
-	}
+		for (s = 0; text_content[s]; s++)
 
 	a = open(filename, O_WRONLY | O_APPEND);
 	b = write(a, text_content, s);
